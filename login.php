0313c4ae37d93809f162c5daa6d08b67ba42aba2
Henry Ho <henrychho89@gmail.com>
2017-10-30 21:46:26 -0700

    Added viewport for scaling

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ session_unset();
         <meta charset="utf-8">
         <title>Math Game</title>
         <link rel="stylesheet" href="css/bootstrap.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <style>
