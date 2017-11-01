ff0eef2fc8245abe9128d128bf14b164005a60be
Henry Ho <henrychho89@gmail.com>
2017-11-01 13:46:01 -0700

    Polished CSS

--- index.php
+++ index.php
@@ -13,42 +13,43 @@ $index = rand(0, sizeof($operator) - 1);
 $answer;
 switch($index) {
     case 0:
-        $answer = $num1 + $num2;
-        break;
+    $answer = $num1 + $num2;
+    break;
     case 1:
-        $answer = $num1 - $num2;
-        break;
+    $answer = $num1 - $num2;
+    break;
     case 2:
-        $answer = $num1 * $num2;
-        break;
+    $answer = $num1 * $num2;
+    break;
 }
 ?>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="./css/style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <title>Math Game</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Math Game</title>
+</head>
 
-    <body>
-        <div class="game-wrapper container-fluid">
-            <div class="row">
-                <div class="col-sm-3"></div>
-                <div class="col-sm-6">
-                    <div class="game-header">
-                        <div class="row">
-                            <div class="col-sm-12 bg-dark">
-                                <h1 class="game-title text-center">Math Game</h1>
-                                <hr>
-                            </div>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <div class="game-header">
+                    <div class="row">
+                        <div class="col-sm-12 bg-dark">
+                            <h1 class="game-title text-center">Math Game</h1>
                         </div>
                     </div>
+                </div>
+                <div class="content">          
                     <div class="game-body">
+                        <hr />
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="col-xs-4">
@@ -68,9 +69,9 @@ switch($index) {
                                 </div>
                             </div>
                         </div>
+                        <hr />
                     </div>
-                    <div class="row">
-                        <hr>
+                    <div class="row">                        
                         <div class="col-sm-12">
                             <form action="./validate.php" method="post">
                                 <div class="form-group">
@@ -116,8 +117,9 @@ switch($index) {
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3"></div>
             </div>
+            <div class="col-sm-3"></div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
--- login.php
+++ login.php
@@ -5,40 +5,35 @@ session_unset();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Math Game</title>
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <style>
-            .error_message {
-                color: red;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Please login to play the math game</h1>
-
+<head>
+    <meta charset="utf-8">
+    <title>Math Game</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Please login to play the math game</h1>
+        <div class="content col-xs-8 col-xs-offset-2">
             <form class="form-horizontal" action="authenticate.php" method="post">
                 <div class="form-group">
                     <div class="row text-center">
-                        <div class="col-xs-3 text-right">
+                        <div class="col-sm-3 text-left">
                             <label for="email">Email: </label>
                         </div>
-                        <div class="col-xs-6 text-left">
+                        <div class="col-sm-8 text-left">
                             <input class="form-control" type="text" name="email" id="email" placeholder="Email" />
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row text-center">
-                        <div class="col-xs-3 text-right">
+                        <div class="col-sm-3 text-left">
                             <label for="password">Password: </label>
                         </div>
-                        <div class="col-xs-6 text-left">
+                        <div class="col-sm-8 text-left">
                             <input class="form-control" type="password" name="password" id="password" placeholder="Password"/>
                         </div>
                     </div>
@@ -47,7 +42,7 @@ session_unset();
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
-            <div class="col-xs-offset-3 error_message">
+            <div class="col-xs-offset-3 error">
                 <?
                 if ($error) {
                     echo "Invalid login credentials";
@@ -55,5 +50,6 @@ session_unset();
                 ?>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
